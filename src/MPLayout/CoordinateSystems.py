61566e1aa1781f3fc2ee0055a630d32ddd043343
Christian Siegl <christian.siegl@gmail.com>
2022-09-11 08:09:59 +0200

    add conversion functions for coordinate systems

--- src/MPLayout/CoordinateSystems.py
+++ src/MPLayout/CoordinateSystems.py
@@ -22,7 +22,7 @@ class VectorCylinder:
     z: float = 0
 
     def __call__(self) -> np.ndarray:
-        return Cylinder2Cart(self).__call__()
+        return cylinder_to_cart(self).__call__()
 
 @dataclass
 class VectorSphere:
@@ -31,21 +31,31 @@ class VectorSphere:
     theta: float = 0
 
     def __call__(self) -> np.ndarray:
-        return Sphere2Cart(self).__call__()
+        return sphere_to_cart(self).__call__()
 
-def Cylinder2Cart(v: VectorCylinder) -> VectorCart:
+def cylinder_to_cart(v: VectorCylinder) -> VectorCart:
     return VectorCart(
         x=v.r*np.cos(v.phi),
         y=v.r*np.sin(v.phi),
         z=v.z
     )
 
-def Sphere2Cart(v: VectorSphere) -> VectorCart:
+def cart_to_cylinder(v: VectorCart) -> VectorCylinder:
+    return VectorCylinder(
+        r=np.sqrt(v.x**2+v.y**2),
+        phi=np.arctan2(v.y, v.x),
+        z=v.z
+    )
+
+def sphere_to_cart(v: VectorSphere) -> VectorCart:
     return VectorCart(
         x=v.r*np.sin(v.theta)*np.cos(v.phi),
         y=v.r*np.sin(v.theta)*np.sin(v.phi),
         z=v.r*np.cos(v.theta)
     )
+
+def cart_vector(v: np.ndarray) -> VectorCart:
+    return VectorCart(x=v[0], y=v[1], z=v[2])
 
 def vector_norm(v: Vector) -> float:
     return np.sqrt(np.sum(v()**2))
